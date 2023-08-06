c61cf6b64459f6378866c2af21420269e97c32a7
Oleg Minovich <oleg.minovich@mail.ru>
2023-08-06 17:03:05 +0300

    deleted not needed tests and checks

--- internal/service/profile-service_test.go
+++ internal/service/profile-service_test.go
@@ -20,38 +20,6 @@ func TestCreateProfile(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestCreateProfileNilProfile(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-	s := NewProfileService(r, &cfg)
-
-	err := s.CreateProfile(context.Background(), nil)
-	require.Error(t, err)
-}
-
-func TestCreateProfileNilID(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-	r.On("CreateProfile", mock.Anything, mock.AnythingOfType("*model.Profile")).Return(nil)
-
-	s := NewProfileService(r, &cfg)
-
-	testProfile.ID = uuid.Nil
-	err := s.CreateProfile(context.Background(), &testProfile)
-	require.Error(t, err)
-}
-
-func TestCreateProfileEmptyUsername(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-	r.On("CreateProfile", mock.Anything, mock.AnythingOfType("*model.Profile")).Return(nil)
-
-	s := NewProfileService(r, &cfg)
-
-	testProfile.ID = uuid.New()
-	testProfile.Username = ""
-	err := s.CreateProfile(context.Background(), &testProfile)
-	testProfile.Username = "Volodya"
-	require.Error(t, err)
-}
-
 func TestGetPasswordAndIDByUsername(t *testing.T) {
 	r := new(mocks.ProfileRepository)
 
@@ -64,18 +32,6 @@ func TestGetPasswordAndIDByUsername(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, len([]byte("pass")), len(hashedPassword))
 	require.Equal(t, testProfile.ID, profileID)
-}
-
-func TestGetPasswordAndIDByEmptyUsername(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-
-	r.On("GetPasswordAndIDByUsername", mock.Anything, mock.AnythingOfType("string")).
-		Return(testProfile.ID, nil, nil)
-
-	s := NewProfileService(r, &cfg)
-
-	_, _, err := s.GetPasswordAndIDByUsername(context.Background(), "")
-	require.Error(t, err)
 }
 
 func TestGetRefreshTokenByID(t *testing.T) {
@@ -91,14 +47,6 @@ func TestGetRefreshTokenByID(t *testing.T) {
 	require.Equal(t, len([]byte("token")), len(hashedToken))
 }
 
-func TestGetRefreshTokenByIDNil(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-	s := NewProfileService(r, &cfg)
-
-	_, err := s.GetRefreshTokenByID(context.Background(), uuid.Nil)
-	require.Error(t, err)
-}
-
 func TestAddRefreshToken(t *testing.T) {
 	r := new(mocks.ProfileRepository)
 
@@ -111,30 +59,6 @@ func TestAddRefreshToken(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestAddRefreshTokenNilToken(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-
-	r.On("AddRefreshToken", mock.Anything, mock.AnythingOfType("[]uint8"), mock.AnythingOfType("uuid.UUID")).
-		Return(nil)
-
-	s := NewProfileService(r, &cfg)
-
-	err := s.AddRefreshToken(context.Background(), nil, testProfile.ID)
-	require.Error(t, err)
-}
-
-func TestAddRefreshTokenNilID(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-
-	r.On("AddRefreshToken", mock.Anything, mock.AnythingOfType("[]uint8"), mock.AnythingOfType("uuid.UUID")).
-		Return(nil)
-
-	s := NewProfileService(r, &cfg)
-
-	err := s.AddRefreshToken(context.Background(), testProfile.RefreshToken, uuid.Nil)
-	require.Error(t, err)
-}
-
 func TestDeleteProfile(t *testing.T) {
 	r := new(mocks.ProfileRepository)
 	r.On("DeleteProfile", mock.Anything, mock.AnythingOfType("uuid.UUID")).
@@ -142,12 +66,4 @@ func TestDeleteProfile(t *testing.T) {
 	s := NewProfileService(r, &cfg)
 	err := s.DeleteProfile(context.Background(), uuid.New())
 	require.NoError(t, err)
-}
-
-func TestDeleteProfileNilID(t *testing.T) {
-	r := new(mocks.ProfileRepository)
-	s := NewProfileService(r, &cfg)
-
-	err := s.DeleteProfile(context.Background(), uuid.Nil)
-	require.Error(t, err)
 }
